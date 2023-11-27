6025601f5766929b02c8e81f5b2770892e01ef82
MacNikSipeykin <niksipeykin@mail.ru>
2023-11-27 20:48:14 +0300

    lesson1 is completed

---
+++ src/main/java/com/java_ee/servlet/lesson1/ProductDisplayerServlet.java
@@ -0,0 +1,25 @@
+package com.java_ee.servlet.lesson1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProductDisplayerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Product[] products = new Product[10];
+        for (int i = 0; i < products.length ; i++) {
+            products[i] = new Product();
+        }
+
+        resp.getWriter().printf("<html><body>");
+
+        for (int i = 0; i < products.length; i++) {
+            resp.getWriter().printf("<h1>" + products[i].getTitle() + "</h1>");
+        }
+        resp.getWriter().printf("</body></html>");
+    }
+}
